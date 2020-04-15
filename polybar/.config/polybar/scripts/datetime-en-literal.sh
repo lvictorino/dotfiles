49609f3ece92d794600fa7baaed9ec01fcc609f3
Laurent Victorino <lvictorino@monkeymoon.net>
2020-04-15 18:27:32 +0200

    Update polybar: date & time written in better English.

--- polybar/.config/polybar/scripts/datetime-en-literal.sh
+++ polybar/.config/polybar/scripts/datetime-en-literal.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Using XX wildcard for sed.
-LC_ALL=C date "+%A %dXX of %B %Y, %H hours and %M minutes" | sed -e 's/11XX/11th/' -e 's/12XX/12th/' -e 's/13XX/13th/' -e 's/1XX/1st/' -e 's/2XX/2nd/' -e 's/3XX/3rd/' -e 's/XX/th/'
+LC_ALL=C date "+%A %dXX %B %Y, %H hours %M minutes" | sed -e 's/11XX/11th/' -e 's/12XX/12th/' -e 's/13XX/13th/' -e 's/1XX/1st/' -e 's/2XX/2nd/' -e 's/3XX/3rd/' -e 's/XX/th/'
