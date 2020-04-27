a1d323e124244968cc4064f5787b1bc741f37b6f
Laurent Victorino <lvictorino@monkeymoon.net>
2020-04-27 08:43:45 +0200

    Update Emacs: i3 reload is hooked on i3 config file save.

--- emacs/.emacs.d/config/hooks.el
+++ emacs/.emacs.d/config/hooks.el
@@ -5,6 +5,16 @@
 )
 (add-hook 'after-save-hook 'emacs-config-reload-hook)
 
+(defun i3-reload-hook()
+  "Reload ~/.config/i3/config on save."
+  (if (cl-search ".config/i3/" (file-name-as-directory buffer-file-name)) 1)
+  (start-process "run i3-msg reload on ~/.config/i3/config saved buffer"
+		 (get-buffer-create "*i3-reload*")
+		 "/usr/bin/i3-msg"
+		 "reload")
+)
+(add-hook 'after-save-hook 'i3-reload-hook)
+
 (defun xresources-reload-hook()
   "Reload ~/.Xresources on save."
   (if (string=(file-name-nondirectory buffer-file-name) ".Xresources")
